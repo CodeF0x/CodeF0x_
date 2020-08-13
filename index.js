ae9cc16070c62aa045c865f8d2286f374b80ecce
CodeF0x <contact@codef0x.dev>
2020-08-13 16:29:00 +0200

    Adjusted code to my needs.

--- index.js
+++ index.js
@@ -1,21 +1,22 @@
-const { Octokit } = require("@octokit/rest");
-const fetch = require("node-fetch");
-const fs = require("fs");
+const { Octokit } = require('@octokit/rest');
+const fetch = require('node-fetch');
+const fs = require('fs');
+const { error } = require('console');
 
 const {
   GIST_ID: gistId,
   GITHUB_TOKEN: githubToken,
   SPOTIFY_CLIENT_SECRET: spotifyClientSecret,
-  SPOTIFY_CLIENT_ID: spotifyClientId
+  SPOTIFY_CLIENT_ID: spotifyClientId,
 } = process.env;
 
 let spotifyCode = process.env.SPOTIFY_CODE;
 
-const API_BASE = "https://api.spotify.com/v1";
-const AUTH_CACHE_FILE = "spotify-auth.json";
+const API_BASE = 'https://api.spotify.com/v1';
+const AUTH_CACHE_FILE = 'spotify-auth.json';
 
 const octokit = new Octokit({
-  auth: `token ${githubToken}`
+  auth: `token ${githubToken}`,
 });
 
 async function main() {
@@ -30,16 +31,16 @@ async function getSpotifyToken() {
   // default env vars go in here (temp cache)
   let cache = {};
   let formData = {
-    grant_type: "authorization_code",
+    grant_type: 'authorization_code',
     code: spotifyCode,
-    redirect_uri: "http://localhost/"
+    redirect_uri: 'http://localhost/',
   };
 
   // try to read cache from disk if already exists
   try {
     const jsonStr = fs.readFileSync(AUTH_CACHE_FILE);
     const c = JSON.parse(jsonStr);
-    Object.keys(c).forEach(key => {
+    Object.keys(c).forEach((key) => {
       cache[key] = c[key];
     });
   } catch (error) {
@@ -49,26 +50,26 @@ async function getSpotifyToken() {
   if (cache.spotifyRefreshToken) {
     console.debug(`ref: ${cache.spotifyRefreshToken.substring(0, 6)}`);
     formData = {
-      grant_type: "refresh_token",
-      refresh_token: cache.spotifyRefreshToken
+      grant_type: 'refresh_token',
+      refresh_token: cache.spotifyRefreshToken,
     };
   }
 
   // get new tokens
-  const data = await fetch("https://accounts.spotify.com/api/token", {
-    method: "post",
+  const data = await fetch('https://accounts.spotify.com/api/token', {
+    method: 'post',
     body: encodeFormData(formData),
     headers: {
-      "Content-Type": "application/x-www-form-urlencoded",
+      'Content-Type': 'application/x-www-form-urlencoded',
       Authorization:
-        "Basic " +
-        new Buffer.from(spotifyClientId + ":" + spotifyClientSecret).toString(
-          "base64"
-        )
-    }
+        'Basic ' +
+        new Buffer.from(spotifyClientId + ':' + spotifyClientSecret).toString(
+          'base64'
+        ),
+    },
   })
-    .then(data => data.json())
-    .catch(error => console.debug(error));
+    .then((data) => data.json())
+    .catch((error) => console.debug(error));
   console.debug(data);
   cache.spotifyAccessToken = data.access_token;
   if (data.refresh_token) {
@@ -83,36 +84,74 @@ async function getSpotifyToken() {
   return cache.spotifyAccessToken;
 }
 
-const encodeFormData = data => {
+const encodeFormData = (data) => {
   return Object.keys(data)
-    .map(key => encodeURIComponent(key) + "=" + encodeURIComponent(data[key]))
-    .join("&");
+    .map((key) => encodeURIComponent(key) + '=' + encodeURIComponent(data[key]))
+    .join('&');
 };
 
 /**
  * Fetches your data from the spotify API
  */
 async function getSpotifyData() {
-  // recent 20 played data (add other endpoints for more info as needed)
-  const recentlyPlayedData = await fetch(
-    `${API_BASE}/me/player/recently-played?limit=20`,
-    {
-      method: "get",
-      headers: {
-        Authorization: "Bearer " + (await getSpotifyToken())
-      }
-    }
-  )
-    .then(data => data.json())
-    .catch(error => console.debug(error));
+  const token = (await getSpotifyToken());
+  // recently most listend song
+  let data = await fetch(`${API_BASE}/me/top/tracks?time_range=short_term&limit=1`, {
+    headers: {
+      Accept: 'application/json',
+      Authorization: `Bearer ${token}`,
+      'Content-Type': 'application/json',
+    },
+  })
+    .then((data) => data.json())
+    .catch((error) => console.error(error));
 
-  return recentlyPlayedData;
+  const recentSongData = {
+    artistName: data.items[0].album.artists[0].name,
+    artistLink: data.items[0].album.artists[0].external_urls.spotify,
+    songName: data.items[0].name,
+    songLink: data.items[0].external_urls.spotify
+  };
+
+  // most listened genre long term
+  data = await fetch(`${API_BASE}/me/top/artists/?time_range=long_term&limit=1`, {
+    headers: {
+      Accept: 'application/json',
+      Authorization: `Bearer ${token}`,
+      'Content-Type': 'application/json'
+    }
+  })
+  .then((data) => data.json())
+  .catch((error) => console.error(error));
+
+  const mostListenedGenre = {
+    genreName: data.items[0].genres[0]
+  };
+
+  // most listened genre short term
+  data = await fetch(`${API_BASE}/me/artists/?time_range=short_term&limit=1`, {
+    headers: {
+      Accept: 'application/json',
+      Authorization: `Bearer ${token}`,
+      'Content-Type': 'application/json'
+    }
+  })
+  .then((data) => data.json())
+  .catch((error) => console.error(error));
+
+  const shortTermGenre = {
+    genreName: data.items[0].genres[0]
+  };
+
+  return {
+    recentSongData,
+    mostListenedGenre,
+    shortTermGenre
+  };
 }
 
 async function updateGist(data) {
   let gist;
-  let songs = [];
-  let lines = "";
 
   try {
     gist = await octokit.gists.get({ gist_id: gistId });
@@ -121,26 +160,12 @@ async function updateGist(data) {
     throw error;
   }
 
-  data.items.forEach(item =>
-    songs.push(`${item.track.name} - ${item.track.artists[0].name}`)
-  );
+  const conent = `
+  Currently, I can't get enough of the song <a href="${data.recentSongData.songLink}">${data.recentSongData.songName}</a> by <a href="${data.recentSongData.artistLink}">${data.recentSongData.artistName}</a> on Spotify.
 
-  console.debug(songs);
-
-  const songListenCount = songs.reduce((prev, curr) => {
-    prev[curr] = (prev[curr] || 0) + 1;
-    return prev;
-  }, {});
-
-  const songList = Object.keys(songListenCount);
-  const songOnRepeat = Object.keys(songListenCount).reduce((a, b) =>
-    songListenCount[a] > songListenCount[b] ? a : b
-  );
-
-  lines += `🎧 On Repeat Recently: ${
-    songListenCount[songOnRepeat] > 2 ? songOnRepeat : "Nothing..."
-  }\n\n`;
-  lines += songList.join("\n");
+  My most listened genre is <a href="https://duckduckgo.com/?q=${data.mostListenedGenre.genreName + 'music'}>${data.mostListenedGenre.genreName}</a>.
+  Still, I've been listening to a lot of <a href="https://duckduckgo.com/?q=${data.shortTermGenre.genreName + 'music'}>${data.shortTermGenre.genreName}</a> lately.
+  `
 
   try {
     const filename = Object.keys(gist.data.files)[0];
@@ -148,10 +173,10 @@ async function updateGist(data) {
       gist_id: gistId,
       files: {
         [filename]: {
-          filename: `activity.md`,
-          content: lines
-        }
-      }
+          filename: `spotify-activity.md`,
+          content: content,
+        },
+      },
     });
   } catch (error) {
     console.error(`Unable to update gist\n${error}`);
